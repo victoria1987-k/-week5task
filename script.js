4442e50c6b49a88dea0ef7a16b5f2052640772b3
victoria1987-k <85343072+victoria1987-k@users.noreply.github.com>
2021-07-09 05:37:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,17 @@
+const firstname=document.getElementById('firstname');
+const lastname=document.getElementById('lastname')
+const password1=document.getElementById('password1')
+const password2=document.getElementById('password2')
+const submitbtn=document.querySelector('#submitbtn')
 
+submitbtn.addEventListener('click',function validate (event){
+    if(firstname.value===lastname.value){
+    alert("first name and last name must not be the same.")
+     event.preventDefault();
+    }
+    if(password1.value !== password2.value){
+        alert("password did not match");
+    return false; }
 
-
-
-const meter=document.getElementById('meter');
-meter.addEventListener('input',unitconverter);
-function unitconverter(e){
-    let meter=e.target.value;
-    document.getElementById('inches').value=meter *30.97;
-    document.getElementById('feet').value=meter *0.305;
-    document.getElementById('yards').value=meter *1.094;}
-
-    
-
-
-    const inches=document.getElementById('inches');
-    inches.addEventListener('input',unitconverter1);
-    function unitconverter1(e){
-        let inches=e.target.value;
-        document.getElementById('meter').value=inches *30.97;
-        document.getElementById('feet').value=inches *0.305;
-        document.getElementById('yards').value=inches *1.094;}
-
-
-        
-
-    const feet=document.getElementById('feet');
-    feet.addEventListener('input',unitconverter2);
-    function unitconverter2(e){
-        let feet=e.target.value;
-        document.getElementById('meter').value=feet *30.97;
-        document.getElementById('inches').value=feet *0.305;
-        document.getElementById('yards').value=feet *1.094;}
-
-
-        
-    const yards=document.getElementById('yards');
-    yards.addEventListener('input',unitconverter3);
-    function unitconverter3(e){
-        let yards=e.target.value;
-        document.getElementById('meter').value=yards *30.97;
-        document.getElementById('inches').value=yards *0.305;
-        document.getElementById('feet').value=yards *1.094;}
-
-
-
-
-
-
-
-
-
+    else{alert('form submitted');}
+})
